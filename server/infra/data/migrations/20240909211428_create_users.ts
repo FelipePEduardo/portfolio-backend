db0d43bf76d7d6aaf39cfcdb5eec90f68d7665ea
FelipePEduardo <Felipeeduardol7@Outlook.com>
2024-09-10 10:55:32 -0300

    ✨ add migrations

---
+++ server/infra/data/migrations/20240909211428_create_users.ts
@@ -0,0 +1,18 @@
+import type { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('users', (table) => {
+    table.increments('id').primary();
+    table.string('name', 100).notNullable();
+    table.string('email', 255).notNullable().unique();
+    table.string('password').notNullable();
+    table.boolean('admin').defaultTo(false);
+    table.datetime('created_at').notNullable();
+    table.datetime('updated_at').defaultTo(null);
+    table.boolean('active').notNullable().defaultTo(true);
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('users');
+}
