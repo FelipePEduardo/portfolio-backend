717966fed074f57c92e91b3eba64c18474ca90c1
FelipePEduardo <Felipeeduardol7@Outlook.com>
2024-10-07 00:32:41 -0300

    ♻️add get request info in user methods

--- server/DTO/User/UserCreateDto.ts
+++ server/DTO/User/UserCreateDto.ts
@@ -1,9 +1,15 @@
 import { z } from 'zod';
 
-export const UserCreateSchema = z.object({
-  name: z.string(),
-  email: z.string().email('Email not valid'),
-  password: z.string().min(6, 'Password must have at least 6 characters'),
-});
+export const UserCreateSchema = z
+  .object({
+    name: z.string(),
+    email: z.string().email('Email not valid'),
+    password: z.string().min(6, 'Password must have at least 6 characters'),
+    confirmPassword: z.string().min(6, 'Password must have at least 6 characters'),
+  })
+  .refine(({ password, confirmPassword }) => password === confirmPassword, {
+    message: "Passwords don't match",
+    path: ['confirmPassword'],
+  });
 
 export type UserCreateDto = z.infer<typeof UserCreateSchema>;
