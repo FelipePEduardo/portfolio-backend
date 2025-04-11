f51afc4af3fb612314ed708c3e8f996e1a6940dd
FelipePEduardo <Felipeeduardol7@Outlook.com>
2025-04-11 12:24:51 -0300

    🐛 fix cors

--- server/server.ts
+++ server/server.ts
@@ -12,7 +12,11 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: true,
+  }),
+);
 app.use(routes);
 
 app.listen(port, async () => {
